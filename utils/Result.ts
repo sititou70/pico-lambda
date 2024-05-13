7bfb82bba945040097e10f09117e826db1ab11ba
Takuya Shizukuishi <sititou70@gmail.com>
2024-05-13 19:32:44 +0900

    refactor: Result type

--- utils/Result.ts
+++ utils/Result.ts
@@ -1,9 +1,10 @@
-export type Result<V, E = unknown> =
-  | { success: true; value: V }
-  | { success: false; error?: E };
+type ResultOk<V> = { success: true; value: V };
+type ResultError<E> = { success: false; error: E };
 
-export const ok = <V, E>(value: V): Result<V, E> => ({ success: true, value });
-export const error = <V, E>(error?: E): Result<V, E> => ({
+export type Result<V, E = unknown> = ResultOk<V> | ResultError<E>;
+
+export const ok = <V>(value: V): ResultOk<V> => ({ success: true, value });
+export const error = <E>(error?: E): ResultError<E | undefined> => ({
   success: false,
   error,
 });
